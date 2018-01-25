79b10035685be6c52e5f785cccdf45da32a0b36b
allyson boone <aboone@totalsim.us>
2018-01-25 14:43:10 -0500

    more print statements

--- routes/api.js
+++ routes/api.js
@@ -105,22 +105,22 @@ var getStl = function(req, res) {
   console.log(req.query)
   if (req.query.partId != null) {
     url = 'https://cad.onshape.com/api/parts/d/' + req.query.documentId +
-    '/w/' + req.query.workspaceId + '/e/' + req.query.elementId +'/partid/'+ req.query.partId + '/stl' +
+    '/w/' + req.query.workspaceId + '/e/' + req.query.stlElementId +'/partid/'+ req.query.partId + '/stl' +
     '?mode=' + 'text'  +
     '&scale=1&units=inch';
     console.log("** STL for partId " + req.query.partId);
   }
   else {
     url = 'https://cad.onshape.com/api/partstudios/d/' + req.query.documentId +
-        '/w/' + req.query.workspaceId + '/e/' + '8fb18477e1ad8cc3bf294351/stl' + //req.query.elementId + '/stl' +
+        '/w/' + req.query.workspaceId + '/e/' + req.query.stlElementId + '/stl' +
         '?grouping=true&scale=1.0&units=inch';
     // url = "https://cad.onshape.com/api/partstudios/d/6eb6949dcb6636f637b99d1f/w/ee76f1dc366e3919f2fe06c5/e/8fb18477e1ad8cc3bf294351/stl?grouping=true&scale=1.0&units=inch"
     console.log("** STL for partId " + req.query.partId);
   }
 
- //if (req.query.angleTolerance !== '' && req.query.chordTolerance !== '') {
- //   url += '&angleTolerance=' + req.query.angleTolerance +'&chordTolerance=' + req.query.chordTolerance;
- // }
+ if (req.query.angleTolerance !== '' && req.query.chordTolerance !== '') {
+    url += '&angleTolerance=' + req.query.angleTolerance +'&chordTolerance=' + req.query.chordTolerance;
+  }
   console.log("url: ")
   console.log(url)
   console.log(req.user.accessToken)
